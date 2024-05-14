39b6418345eaada0f3ca81cc2a4b7c9fc46582ff
dducktai <dangductai6410@gmail.com>
2024-05-14 13:09:18 +0700

    thêm đề xuất tăng/giảm cân

--- main/VigorWeb/site1/static/site1/js/tools.js
+++ main/VigorWeb/site1/static/site1/js/tools.js
@@ -7,6 +7,7 @@ document.getElementById("Form").addEventListener("submit", function (event) {
     var gender = parseInt(document.querySelector('input[name="phai"]:checked').value);
     var activity = parseInt(document.querySelector('input[name="cuong-do"]:checked').value);
 
+    //Tính BMR
     var bmr;
     if (gender === 1) {
         bmr = 66 + (13.7 * weight) + (5 * height) - (6.8 * age);
@@ -14,6 +15,7 @@ document.getElementById("Form").addEventListener("submit", function (event) {
         bmr = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age);
     }
 
+    //Tính TDEE
     var tdee;
     switch(activity) {
         case 1:
@@ -33,10 +35,44 @@ document.getElementById("Form").addEventListener("submit", function (event) {
             break;
     }
 
+    //Tính BMI
     var bmi;
     height /= 100;
     bmi=weight/(height*height);
+
+    var bmiNotification = document.getElementById("BMInotification");
+    var category, color;
+
+    if (bmi < 16) {
+        category = "Gầy độ III \n Nên tăng cân";
+        color = "#00CCFF";
+    } else if (bmi < 17) {
+        category = "Gầy độ II \n Nên tăng cân";
+        color = "#00CCFF";
+    } else if (bmi < 18.5) {
+        category = "Gầy độ I \n Nên tăng cân";
+        color = "#00CCFF";
+    } else if (bmi < 25) {
+        category = "Bình thường";
+        color = "green";
+    } else if (bmi < 30) {
+        category = "Thừa cân \n Nên giảm cân";
+        color = "#FFCC33";
+    } else if (bmi < 35) {
+        category = "Béo phì độ I \n Nên giảm cân";
+        color = "orange";
+    } else if (bmi < 40) {
+        category = "Béo phì độ II \n Nên giảm cân";
+        color = "orangered";
+    } else {
+        category = "Béo phì độ III \n Nên giảm cân";
+        color = "red";
+    }
+
+    bmiNotification.innerText = category;
+    bmiNotification.style.color = color;
     
+    //Tính bảng tăng/giảm cân
     var dec1, dec2, dec3, inc1, inc2, inc3;
 
     dec1 = 0.85 * tdee;
