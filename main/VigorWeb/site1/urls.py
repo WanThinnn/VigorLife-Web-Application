d47ff113127bd2604d3359e064bce70a0a148a00
WanThinnn <thienlai159@gmail.com>
2024-05-15 11:59:21 +0700

    check tien do

--- main/VigorWeb/site1/urls.py
+++ main/VigorWeb/site1/urls.py
@@ -8,19 +8,17 @@ from django.conf.urls.static import static
 from .views import *
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('',views.home,name='home'),
-    path('introduction/',views.introduction,name='introduction'),
-    path('heallthinfo/',views.heallthinfo,name='heallthinfo'),
-    path('loseweight/',views.loseweight,name='loseweight'),
-    path('tools/',views.tools,name='tools'),
-    path('login/',views.loginPage,name='login'),
-    path('logout/',views.logoutPage,name='logout'),
-    path('register/',views.register,name='register'),
-    path('verify/',views.verifyOTP,name='verify'),
-    path('social-auth/', include('social_django.urls', namespace='social')),
-]
-=======
+#     path('',views.home,name='home'),
+#     path('introduction/',views.introduction,name='introduction'),
+#     path('heallthinfo/',views.heallthinfo,name='heallthinfo'),
+#     path('loseweight/',views.loseweight,name='loseweight'),
+#     path('tools/',views.tools,name='tools'),
+#     path('login/',views.loginPage,name='login'),
+#     path('logout/',views.logoutPage,name='logout'),
+#     path('register/',views.register,name='register'),
+#     path('verify/',views.verifyOTP,name='verify'),
+#     path('social-auth/', include('social_django.urls', namespace='social')),
+# ]
     path('', views.home, name='home'),
     path('', views.autocomplete, name='autocomplete'),
     path('introduction/', views.introduction, name='introduction'),
@@ -51,6 +49,7 @@ urlpatterns = [
     path('foods/<str:classification>/<str:name>', views.FoodsPage, name='foodspage'),
     
     path('news/', views.News, name='news'), 
+    path('social-auth/', include('social_django.urls', namespace='social')),
     
 ]
 
@@ -58,4 +57,3 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
->>>>>>> be7edeb2e004977e29034d458d7a809a521f2900
