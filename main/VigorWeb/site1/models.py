8a17cd6da12e39edd11fe493c26ac2cbb8864bbc
WanThinnn <thienlai159@gmail.com>
2024-05-23 14:18:17 +0700

    them truong du lieu cho Foods

--- main/VigorWeb/site1/models.py
+++ main/VigorWeb/site1/models.py
@@ -98,6 +98,7 @@ class Food(models.Model):
         max_length=50, choices=CLASSIFICATION_CHOICES)
     # Hình ảnh của trái cây
     image = models.ImageField(upload_to='foods_images/')
+    unit = models.CharField(max_length=50, null=True)
 
     def __str__(self):
         return self.name
